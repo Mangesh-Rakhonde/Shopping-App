57fdd15a6eab79f5c171362f9aee4e85ea3c93f4
Mangesh-Rakhonde <mangeshrakhonde1997@gmail.com>
2024-03-05 14:47:55 +0530

    React App added to git repo

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1>Hello Buddy </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
