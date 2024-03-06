10398b1087a8eaf49deb4ee517b0f45266671bad
Mangesh-Rakhonde <mangeshrakhonde1997@gmail.com>
2024-03-06 20:03:19 +0530

    Code pushed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 
 import './App.css';
 
+import Nav from './Component/Nav';
+import Products from './Component/Products';
+import Cart from './Component/Cart';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       <h1>Hello Buddy </h1>
-      </header>
+    <div >
+      <BrowserRouter>
+    
+      <Routes>
+      <Route path="/" element={<Nav />} > 
+        <Route index element={<Products />} />   
+        <Route path="cart-items" element={<Cart />} />   
+      </Route>
+      </Routes>
+      
+      </BrowserRouter>
+   
+         
+      
+      
+      
     </div>
   );
 }
